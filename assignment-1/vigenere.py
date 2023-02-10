bc7c1e8a9f5b7a788518c8092df5b87110789bbc
Mihir Chaturvedi <plibither8@gmail.com>
2023-02-10 21:31:26 +0530

    add brute force

--- assignment-1/vigenere.py
+++ assignment-1/vigenere.py
@@ -1,6 +1,7 @@
 import itertools
 import string
 from random import choice
+from tqdm import tqdm
 
 PLAINTEXT_LENGTH = 512  # Always a multiple of N
 N = 8  # Number of columns in the hash table
@@ -13,9 +14,9 @@ def encrypt(plaintext, key):
     ciphertext = ""
     for letter in plaintext:
         if letter in string.ascii_lowercase:
-            ciphertext += chr(ord('a') + (
-                (next(key_iter) - ord('a') + ord(letter) -
-                    ord('a')) % 26))
+            ciphertext += chr(
+                ord("a") + ((next(key_iter) - ord("a") + ord(letter) - ord("a")) % 26)
+            )
         else:
             ciphertext += letter
     return ciphertext
@@ -27,8 +28,9 @@ def decrypt(ciphertext, key):
     decrypted = ""
     for letter in ciphertext:
         if letter in string.ascii_lowercase:
-            decrypted += chr(ord('a') + (ord(letter) - ord('a') -
-                             (next(key_iter) - ord('a'))) % 26)
+            decrypted += chr(
+                ord("a") + (ord(letter) - ord("a") - (next(key_iter) - ord("a"))) % 26
+            )
         else:
             decrypted += letter
     return decrypted
@@ -45,24 +47,38 @@ def is_recognizable(plaintext):
 
 def hash_fn(plaintext):
     # Return a plaintext p that satisfies p = (s, Hash(s))
-    p_hash = ['' for _ in range(M)]
+    p_hash = ["" for _ in range(M)]
     for i in range(M):
         p_hash[i] = plaintext[i * N]
         for j in range(1, N):
-            p_hash[i] = chr(ord('a') + (ord(p_hash[i]) - ord('a') +
-                            ord(plaintext[i * N + j]) - ord('a')) % 26)
-    return ''.join(p_hash)
+            p_hash[i] = chr(
+                ord("a")
+                + (ord(p_hash[i]) - ord("a") + ord(plaintext[i * N + j]) - ord("a"))
+                % 26
+            )
+    return "".join(p_hash)
 
 
 def brute_force_solve(ciphertext, key_length):
-    pass
+    for key in tqdm(
+        itertools.product(string.ascii_lowercase, repeat=key_length),
+        total=26**key_length,
+    ):
+        key = "".join(key)
+        if is_recognizable(decrypt(ciphertext, key)):
+            return key
+    return None
 
 
 if __name__ == "__main__":
-    plaintexts = [(''.join(choice(string.ascii_lowercase)
-                           for _ in range(PLAINTEXT_LENGTH))) for _ in range(5)]
+    plaintexts = [
+        ("".join(choice(string.ascii_lowercase) for _ in range(PLAINTEXT_LENGTH)))
+        for _ in range(5)
+    ]
     plaintexts = [(p + hash_fn(p)) for p in plaintexts]
     print([is_recognizable(p) for p in plaintexts])
-    ciphertexts = [encrypt(p, "abcd") for p in plaintexts]
-    decrypted = [decrypt(c, "abcd") for c in ciphertexts]
+    ciphertexts = [encrypt(p, "zydf") for p in plaintexts]
+    decrypted = [decrypt(c, "zydf") for c in ciphertexts]
+    solved = [brute_force_solve(c, 4) for c in ciphertexts]
+    print(solved)
     print([plaintexts[i] == decrypted[i] for i in range(len(plaintexts))])
