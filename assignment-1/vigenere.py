11d548809656dca15a7873e7baf954d4fe9396e4
Ananya Lohani <ananya19018@iiitd.ac.in>
2023-02-10 17:04:28 +0530

    feat: Add hashing

--- assignment-1/vigenere.py
+++ assignment-1/vigenere.py
@@ -1,5 +1,8 @@
 import itertools
 import string
+from random import choice
+
+PLAINTEXT_LENGTH = 6
 
 
 def encrypt(plaintext, key):
@@ -24,11 +27,32 @@ def decrypt(ciphertext, key):
     return encrypt(ciphertext, key_inv)
 
 
+def is_recognizable(plaintext):
+    # Return True if plaintext p satisfies p = (s, Hash(s))
+    hash_s = plaintext[-1]
+    s = plaintext[:-1]
+    if hash_fn(s) == hash_s:
+        return True
+    return False
+
+
+def hash_fn(plaintext):
+    # Return a plaintext p that satisfies p = (s, Hash(s))
+    p_hash = plaintext[0]
+    for letter in plaintext[1:]:
+        p_hash = chr(ord('a') + (ord(letter) - ord('a') +
+                                 ord(p_hash[-1]) - ord('a')) % 26)
+    return p_hash
+
+
 def brute_force_solve(ciphertext, key_length):
     pass
 
 
 if __name__ == "__main__":
-    c1 = encrypt("hello hi how are you", "abcd")
-    p1 = decrypt(c1, "abcd")
-    print(c1, p1)
+    plaintexts = [(''.join(choice(string.ascii_lowercase)
+                  for i in range(PLAINTEXT_LENGTH))) for j in range(5)]
+    plaintexts = [(p + hash_fn(p)) for p in plaintexts]
+    ciphertexts = [encrypt(p, "abcd") for p in plaintexts]
+    decrypted = [decrypt(c, "abcd") for c in ciphertexts]
+    print([plaintexts[i] == decrypted[i] for i in range(len(plaintexts))])
