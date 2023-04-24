cd5a96eb6706ed8a5ae959175eb754500cbab628
Ananya Lohani <ananya19018@iiitd.ac.in>
2023-04-24 18:28:05 +0530

    feat: Add client code for certificates

---
+++ assignment-4/client/src/pages/certificate.tsx
@@ -0,0 +1,77 @@
+import { PDFViewer } from '@/components/PDFViewer';
+import { Inter } from 'next/font/google';
+import { parseCookies } from 'nookies';
+import React from 'react';
+
+type Props = {};
+
+const inter = Inter({ subsets: ['latin'] });
+
+interface Student {
+  name: string;
+  rollNumber: string;
+}
+
+export default function Certificate({}: Props) {
+  const [student, setStudent] = React.useState<Student>({
+    name: '',
+    rollNumber: '',
+  });
+  const [buffer, setBuffer] = React.useState<Uint8Array | null>(null);
+
+  const cookies = parseCookies(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(
+      'http://localhost:5000/certificate?name=' +
+        student.name +
+        '&rollNumber=' +
+        student.rollNumber,
+      {
+        headers: {
+          Authorization: `JWT ${cookies.token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log({ data });
+        setTimeout(() => setBuffer(new Uint8Array(data.file.data)), 1000);
+      });
+  };
+
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+    >
+      <h1 className='text-3xl font-bold'>Certificate</h1>
+      {buffer ? (
+        <PDFViewer buffer={buffer} />
+      ) : (
+        <form className='flex flex-col mt-8 space-y-2' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Name'
+            className='px-2 py-1 border border-gray-200 rounded'
+            onChange={(e) => setStudent({ ...student, name: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Roll Number'
+            className='px-2 py-1 border border-gray-200 rounded'
+            onChange={(e) =>
+              setStudent({ ...student, rollNumber: e.target.value })
+            }
+          />
+          <button
+            type='submit'
+            className='px-2 py-1 text-white bg-blue-500 rounded'
+          >
+            Request Certificate
+          </button>
+        </form>
+      )}
+    </div>
+  );
+}
--- assignment-4/client/src/pages/sign.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-export default function Sign({}: Props) {
-  return <div>Sign</div>;
-}
